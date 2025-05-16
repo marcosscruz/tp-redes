a5f9e871408b02d65ab26653ab07fd7a748d6f44
JV-OF <joaovitorof@outlook.com>
2025-05-16 16:44:59 -0300

    adição de comentarios

--- ex2_udp_echo/server.py
+++ ex2_udp_echo/server.py
@@ -4,42 +4,47 @@
 import socket
 
 # Configuração do servidor
-HOST = ''  # Endereço vazio = todos os interface
+HOST = '127.0.0.1'  # Aceita apenas conexões da própria máquina (localhost)
 PORT = 6000  # Porta para execurar
 MAX_SIZE = 65507  # Tamanho máximo UDP (64KB - cabeçalho)
-TIMEOUT = 10  # 10 segundos sem atividade
+TIMEOUT = 15  # 15 segundos sem atividade
 
 
 def main():
-    # cria sockect UDP (SOCK_DGRAM)
+    # cria sockect UDP (SOCK_DGRAM) usando IPv4 (AF_INET)
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.bind((HOST, PORT))
+        s.bind((HOST, PORT))  # Associa socket ao endereço e porta
+
         s.settimeout(TIMEOUT)  # Configura timeout
         print(
             f"Servidor UDP ouvindo em {PORT}... Encerrando após {TIMEOUT} segundos de inatividade.")
 
         try:
+            # Loop principal do servidor
             while True:
                 try:
                     # recebe dados e endereço do cliente
                     data, addr = s.recvfrom(MAX_SIZE)
+                    # Decodifica mensagem UTF-8
                     mensagem = data.decode('utf-8')
+                    # Exibe mensagem recebida
                     print(f"Recebido de {addr}: {mensagem}")
                     # envia ECO de volta
                     s.sendto(data, addr)
 
-                #tratamento de erros
-                except socket.timeout:              # Mata o processo para evitar zumbis
+                # tratamento de erros
+                except socket.timeout:                                          # Timeout de inatividade
                     print(
                         f"Nenhuma mensagem recebida em {TIMEOUT} segundos. Encerrando...")
                     break
-                except Exception as e:              # Permite que o servidor não quebre em caso de erros genericos
+                except Exception as e:                                          # Erros genéricos
                     print(f"Erro inesperado: {e}")
                     break
 
-        except KeyboardInterrupt:
-            print("Servidor ocioso interrompido.")
+        except KeyboardInterrupt:                                               # Captura interrupção por teclado
+            print("Servidor interrompido.")
 
 
 if __name__ == "__main__":
+    # Ponto de entrada do programa
     main()
